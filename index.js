4bd60f92e9253896a9775f203b20818993cdf395
omarwebdeve <omarf977865@gmail.com>
2022-11-10 09:36:37 +0600

    delete revies

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete api
+    app.delete('/reviews/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const reviews = await reviewCollection.deleteOne(query)
+      res.send(reviews)
+  })
+
   }
   finally { }
 
